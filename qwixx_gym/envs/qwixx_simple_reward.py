d40b4446630405d43f8ba30247355361ae11e08f
Evan Richards <richards@uber.com>
2020-11-18 16:05:18 -0600

    make default return customizable

--- qwixx_gym/envs/qwixx_simple_reward.py
+++ qwixx_gym/envs/qwixx_simple_reward.py
@@ -1,16 +1,14 @@
 from qwixx_gym.envs import QwixxOneHotEnv
 
 class QwixxSimple(QwixxOneHotEnv):
-    def __init__(self, num_players=3, bot_player=0, invalid_move_reward=-1):
+    def __init__(self, num_players=3, bot_player=0, invalid_move_reward=-1, default_return=0):
         super(QwixxSimple, self).__init__(num_players, bot_player, invalid_move_reward)
-        self.win_reward = 0
-        self.lose_reward = 0
-        self.skip_bias = 0
+        self.default_return = default_return
 
     def calculate_skip_reward(self):
-        return 0
+        return self.default_return
 
     def calculate_reward(self, changed_values, current_score):
         if self._is_done():
             return self._calculate_score()
-        return 0
+        return self.default_return
