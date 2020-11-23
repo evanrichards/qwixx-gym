387efbdbc0511df999a98b399ff7f61c8ceb64b2
Evan Richards <richards@uber.com>
2020-11-22 22:05:47 -0600

    Remove conditional in flat stetp

--- qwixx_gym/envs/qwixx_flat.py
+++ qwixx_gym/envs/qwixx_flat.py
@@ -1,10 +1,8 @@
 from qwixx_gym.envs import QwixxSimple
-from gym.spaces import Discrete
+from gym.spaces import Discrete, MultiDiscrete
 import numpy as np
 
 class QwixxFlat(QwixxSimple):
     action_space = Discrete(45)
     def step(self, action):
-        if not super(QwixxFlat, self).action_space.contains(action):
-            action = np.array([action % 5, action // 5])
-        return super(QwixxFlat, self).step(action)
+        return super(QwixxFlat, self).step(np.array([action % 5, action // 5]))
